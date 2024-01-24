fc7aa722e3650892b744de623c6b6a9384f3728e
DusanBibin <96448632+DusanBibin@users.noreply.github.com>
2024-01-24 01:00:45 +0100

    more service tests

--- MobilneTestiranjeBackend/src/test/java/com/example/mobilnetestiranjebackend/service/ReservationServiceTest.java
+++ MobilneTestiranjeBackend/src/test/java/com/example/mobilnetestiranjebackend/service/ReservationServiceTest.java
@@ -1,12 +1,17 @@
 package com.example.mobilnetestiranjebackend.service;
 
 
+import com.example.mobilnetestiranjebackend.DTOs.ReservationDTO;
 import com.example.mobilnetestiranjebackend.enums.ReservationStatus;
 import com.example.mobilnetestiranjebackend.model.Accommodation;
+import com.example.mobilnetestiranjebackend.model.AccommodationAvailability;
+import com.example.mobilnetestiranjebackend.model.Guest;
 import com.example.mobilnetestiranjebackend.model.Reservation;
+import com.example.mobilnetestiranjebackend.repositories.AccommodationRepository;
 import com.example.mobilnetestiranjebackend.repositories.ReservationRepository;
 import com.example.mobilnetestiranjebackend.services.ReservationService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -20,6 +25,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +34,9 @@ import org.junit.jupiter.api.Test;
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 public class ReservationServiceTest {
+
+    @Mock
+    private AccommodationRepository accommodationRepository;
     @Mock
     private ReservationRepository reservationRepository;
 
@@ -152,5 +161,97 @@ public class ReservationServiceTest {
     }
 
 
-    
+    @Test
+    void testAcceptedReservationRangeTaken_NoConflict() {
+        LocalDate startDate = LocalDate.now().minusDays(5);
+        LocalDate endDate = LocalDate.now().plusDays(8);
+        Long accomId = 1L;
+        Long availId = 2L;
+
+        List<Reservation> acceptedReservations = new ArrayList<>();
+        Mockito.when(reservationRepository.findAcceptedReservationsInConflict(startDate, endDate, accomId, availId))
+                .thenReturn(acceptedReservations);
+
+        boolean isRangeAvailable = reservationService.acceptedReservationRangeTaken(startDate, endDate, accomId, availId);
+
+        Mockito.verify(reservationRepository).findAcceptedReservationsInConflict(startDate, endDate, accomId, availId);
+
+        assertThat(isRangeAvailable).isTrue();
+    }
+
+
+    @Test
+    void testAcceptedReservationRangeTaken_Conflict() {
+        LocalDate startDate = LocalDate.now().minusDays(5);
+        LocalDate endDate = LocalDate.now().plusDays(8);
+        Long accomId = 1L;
+        Long availId = 2L;
+        var reservationConflict = Reservation.builder().build();
+
+
+
+        List<Reservation> acceptedReservations = Arrays.asList(reservationConflict);
+        Mockito.when(reservationRepository.findAcceptedReservationsInConflict(startDate, endDate, accomId, availId))
+                .thenReturn(acceptedReservations);
+
+        boolean isRangeAvailable = reservationService.acceptedReservationRangeTaken(startDate, endDate, accomId, availId);
+
+        Mockito.verify(reservationRepository).findAcceptedReservationsInConflict(startDate, endDate, accomId, availId);
+
+        assertThat(isRangeAvailable).isFalse();
+    }
+
+    @Test
+    void testCreateNewReservationAutoAcceptEnabled() {
+        // Given
+        ReservationDTO request = ReservationDTO.builder().build();
+        Guest guest = Guest.builder().build();
+        Accommodation accom = Accommodation.builder()
+                .autoAcceptEnabled(true)
+                .reservations(new ArrayList<>())
+                .build();
+        AccommodationAvailability avail = AccommodationAvailability.builder().build();
+
+        Mockito.when(accommodationRepository.save(accom)).thenReturn(accom);
+
+        reservationService.createNewReservation(request, guest, accom, avail);
+
+        Mockito.verify(reservationRepository).save(Mockito.any(Reservation.class));
+        Mockito.verify(accommodationRepository).save(accom);
+
+
+        ArgumentCaptor<Reservation> reservationCaptor = ArgumentCaptor.forClass(Reservation.class);
+        Mockito.verify(reservationRepository).save(reservationCaptor.capture());
+        Reservation savedReservation = reservationCaptor.getValue();
+
+        // Assert that the enum type is as expected
+        assertEquals(ReservationStatus.ACCEPTED, savedReservation.getStatus());
+    }
+
+    @Test
+    void testCreateNewReservationAutoAcceptDisabled() {
+        // Given
+        ReservationDTO request = ReservationDTO.builder().build();
+        Guest guest = Guest.builder().build();
+        Accommodation accom = Accommodation.builder()
+                .autoAcceptEnabled(false)
+                .reservations(new ArrayList<>())
+                .build();
+        AccommodationAvailability avail = AccommodationAvailability.builder().build();
+
+        Mockito.when(accommodationRepository.save(accom)).thenReturn(accom);
+
+        reservationService.createNewReservation(request, guest, accom, avail);
+
+        Mockito.verify(reservationRepository).save(Mockito.any(Reservation.class));
+        Mockito.verify(accommodationRepository).save(accom);
+
+
+        ArgumentCaptor<Reservation> reservationCaptor = ArgumentCaptor.forClass(Reservation.class);
+        Mockito.verify(reservationRepository).save(reservationCaptor.capture());
+        Reservation savedReservation = reservationCaptor.getValue();
+
+        // Assert that the enum type is as expected
+        assertEquals(ReservationStatus.PENDING, savedReservation.getStatus());
+    }
 }
